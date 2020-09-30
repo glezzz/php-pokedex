3727b46f4c63220c7c730ce3928887f337cc0f00
glezzz <alexglezk@gmail.com>
2020-09-30 10:34:00 +0200

    display id working on moves

--- pokedex.php
+++ pokedex.php
@@ -5,8 +5,7 @@ ini_Set('display_errors', 1);    // 1 true 0 false
 ini_Set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$pokemon = $_POST['id'];
-
+$pokemon = $_POST['nameid'];
 
 if ($pokemon === null) {
     $pokemon = 1;
@@ -16,15 +15,15 @@ $getData = file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $pokemon);
 $data = (json_decode($getData, true));
 //var_dump($data);
 $name = $data['name'];
+$pokeId = $data['id'];
 $img = $data['sprites']['front_default'];
-echo $name;
 
 
 
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
     <body>
     <div id = "form">
         <form action = "pokedex.php" method = "post">
@@ -33,10 +32,17 @@ echo $name;
         </form>
     </div>
     <div class="name">
-        Name <?php echo $name;?>
+        <?php echo $pokeId; echo '&nbsp;'; echo $name;?>        <!-- &nbsp (Non-Breakable Space) whitespace between id & name-->
     </div>
     <div id="image">
         <img src="<?php echo $img;?>">
     </div>
+    <h3>4 Moves</h3>
+    <ul class="moves">
+        <li id="moveOne"></li>
+        <li id="moveTwo"></li>
+        <li id="moveThree"></li>
+        <li id="moveFour"></li>
+    </ul>
     </body>
 </html>
