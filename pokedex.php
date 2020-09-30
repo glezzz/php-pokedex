573934abfd4925b20c67943adb25c7261efb509d
glezzz <alexglezk@gmail.com>
2020-09-30 10:05:43 +0200

    display pokemon works

--- pokedex.php
+++ pokedex.php
@@ -1,17 +1,23 @@
 <?php
 //declare(strict_types = 1);
 
-ini_Set('display_errors', "1");    // 1 true 0 false
-ini_Set('display_startup_errors', "1");
+ini_Set('display_errors', 1);    // 1 true 0 false
+ini_Set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$pokemon = $_POST['id'];
 
 
-$getData = file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $_GET['nameid']);
-$data = json_decode($getData, true);
-var_dump($data);
+if ($pokemon === null) {
+    $pokemon = 1;
+}
+
+$getData = file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $pokemon);
+$data = (json_decode($getData, true));
+//var_dump($data);
 $name = $data['name'];
 $img = $data['sprites']['front_default'];
+echo $name;
 
 
 
