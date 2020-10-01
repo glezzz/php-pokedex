5fa4da8b6a9be8ff2c678c89a6b69bb50ecf20e3
glezzz <alexglezk@gmail.com>
2020-10-01 10:17:05 +0200

    refactoring var names

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
-//declare(strict_types = 1);
+declare(strict_types = 0);
 
 ini_Set('display_errors', 1);    // 1 true 0 false
 ini_Set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 global $pokemon;
-$pokemon = $_POST['nameid'];       // get input from form
-
-if ($pokemon === null) {
-    $pokemon = 1;
+                                // get input from form
+if(isset($_GET['nameid'])){
+    $pokemon = $_GET['nameid'];
 }
+
+
 //fetch data
 $getData = file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $pokemon);
 $data = (json_decode($getData, true));
@@ -20,17 +21,15 @@ $pokeId = $data['id'];
 $img = $data['sprites']['front_default'];
 $allMoves = $data['moves'];
 
-
 //get moves
-function getMoves($allMoves)
-{
+function getMoves($allMoves){
 
     $arrFourRandMoves = array();
     $maxMoves = count($allMoves);   //count() count all elements in array
     if ($maxMoves > 4) {
         $fourMoves = 4;
-        //only pick 4 moves but if any pokemon has fewer than 4, show them all
-    } elseif ($maxMoves < 4) {      // e.g. Ditto
+                                    //only pick 4 moves but if any pokemon has fewer than 4, show them all
+    } else {                        // e.g. Ditto
         $fourMoves = $maxMoves;
     }
 
@@ -52,21 +51,26 @@ $displayMoves = getMoves($allMoves);        // make a var out of the func to use
 
 function getEvo($pokemon){
                                 //fetch evolution data
-    $getEvoData = file_get_contents('https://pokeapi.co/api/v2/pokemon-species/' . $pokemon);
-    $evoData = (json_decode($getEvoData, true));
-    $prevEvo = $evoData['evolves_from_species'];        
+    $get_evo_data = file_get_contents('https://pokeapi.co/api/v2/pokemon-species/' . $pokemon);
+    $evo_data = (json_decode($get_evo_data, true));
+    $prev_evo = $evo_data['evolves_from_species'];
 
-        $prevEvoName = $prevEvo['name'];            //fetch data of previous evolution
-        $getPrevEvoData = file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $prevEvoName);
-        $prevEvoData = (json_decode($getPrevEvoData, true));
-        $prevEvoImg = $prevEvoData['sprites']['front_default'];     //get sprite from previous evolution
+    if($prev_evo) {         // if pokemon has a previous evolution,
+        $prev_evo_name = $prev_evo['name'];            //fetch data of previous evolution
+        $get_prev_evo_data = file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $prev_evo_name);
+        $prev_evo_data = (json_decode($get_prev_evo_data, true));
+        $prev_evo_img = $prev_evo_data['sprites']['front_default'];     //get sprite from previous evolution
+        return($prev_evo_img);
 
-    return($prevEvoImg);
+
+    }else{
+
+    }
+
 }
 
-
 getEvo($pokemon);
-$prevEvoImg = getEvo($pokemon);     // we need to use it in HTML img tag
+$prev_evo_img = getEvo($pokemon);     // we need to use it in HTML img tag
 
 
 
@@ -75,7 +79,7 @@ $prevEvoImg = getEvo($pokemon);     // we need to use it in HTML img tag
 <html lang="en" xmlns="http://www.w3.org/1999/html">
 <body>
 <div id="form">
-    <form action="index.php" method="post">
+    <form action="index.php" method="get">
         Pokemon: <input type="text" name="nameid"/>
         <input type="submit"/>
     </form>
@@ -99,7 +103,7 @@ $prevEvoImg = getEvo($pokemon);     // we need to use it in HTML img tag
     ?>
 </ul>
 <h3>Previous Evolution</h3>
- <img src="<?php echo $prevEvoImg; ?>">
+ <img src="<?php echo $prev_evo_img; ?>">
 
 </body>
 </html>
